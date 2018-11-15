eabf341f4c714e1a177582eb22208e9d0f75a7e9
ArnaudDauguen <arnaud.dauguen@orange.fr>
2018-11-15 12:44:32 +0100

    idée pour delQuestion

--- delQuestion.js
+++ delQuestion.js
@@ -6,61 +6,22 @@ exports.delQuestion = function delQuestion(){
     let promise = Promise.resolve()
     .then(() => {
       //inputs
-      //formulaire
-      inputs = [
-        {
-          type: 'checkbox',
-          message: 'séléctionnez le thème de la réponse',
-          name:'0',
-          choices: ['1', '2', '3']
-        },
-        {
-          type: 'input',
-          message: 'Veuillez entrer la question',
-          name:'1'
-        },
-        {
-          type: 'input',
-          message: 'Veuillez entrer la 1ere réponse',
-          name:'2'
-        },
-        {
-          type: 'input',
-          message: 'Veuillez entrer la 2nd réponse',
-          name:'3'
-        },
-        {
-          type: 'input',
-          message: 'Veuillez entrer la 3eme réponse',
-          name:'4'
-        },
-        {
-          type: 'checkbox',
-          message: 'séléctionnez la bonne réponse',
-          name:'5',
-          choices: ['1', '2', '3']
-        }
-      ]
-      return inputs
-    })
-    .then((inputs) => {
-      //poser les question de formation de nouvelle question
-      return inquirer.prompt(inputs)
+      return inquirer.prompt()
      })
      .then((answers) => {      
       //on range les réponses
-      let nouvelleQuestion = {
+      let questionToDelete = {
        "theme" : parseInt(answers[0]),
        "question" : answers[1],
        "reponses" : [answers[2], answers[3], answers[4]],
        "goodAnswer" : parseInt(answers[5][0]-1)
       }
-      return nouvelleQuestion
+      return questionToDelete
     })
   
-    .then((nouvelleQuestion) => {
+    .then((questionToDelete) => {
       let anciantQuestionList = require('./questions.json');
-      anciantQuestionList.push(nouvelleQuestion)
+      anciantQuestionList.push(questionToDelete)
       writeData(anciantQuestionList)
     })
   
